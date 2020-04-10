a2b6dd47399e6e17e580350b14e1652e4caec49e
Wilver Deypalubos <mephistophelles1994@gmail.com>
2020-04-10 21:00:03 +0800

    Services Added and API integration for signin

--- src/app/components/landing-page/login/login.component.ts
+++ src/app/components/landing-page/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { ApiHostService } from '../../../services/api-host.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public signInForm: FormGroup;
+  constructor(
+    private apiHost: ApiHostService,
+    private fb: FormBuilder,
+
+  ) {
+    this.signInModel();
+  }
 
   ngOnInit(): void {
+  }
+  signInModel() {
+    this.signInForm = this.fb.group({
+      username: [null, Validators.required],
+      password: [null, Validators.required]
+    })
+  }
+
+  onSubmit() {
+    console.log(this.signInForm.value);
+    const { value } = this.signInForm;
+
+    this.apiHost.signin(value).subscribe((response: any) => {
+      if (response) {
+        console.log(response);
+      }
+    })
+
   }
 
 }
