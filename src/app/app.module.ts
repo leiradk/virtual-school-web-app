1724452934a3469342daa77c4f920cb091c1e9b3
Wilver Deypalubos <mephistophelles1994@gmail.com>
2020-04-10 21:00:03 +0800

    merged conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
-import { NgxPaginationModule } from 'ngx-pagination';
+import { BrowserModule } from "@angular/platform-browser";
+// import { Http, HttpModule } from "@angular/http";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Ng2SearchPipeModule } from "ng2-search-filter";
+import { NgxPaginationModule } from "ngx-pagination";
 
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { LoginComponent } from './components/landing-page/login/login.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { TeacherStaffComponent } from './components/dashboard/content/list-page/teacher-staff/teacher-staff.component';
-import { ListPageComponent } from './components/dashboard/content/list-page/list-page.component';
-import { StudentsComponent } from './components/dashboard/content/list-page/students/students.component';
-import { ContentComponent } from './components/dashboard/content/content.component';
-import { NavbarComponent } from './components/dashboard/navbar/navbar.component';
-import { SidebarComponent } from './components/dashboard/sidebar/sidebar.component';
-import { MainPageComponent } from './components/dashboard/content/main-page/main-page.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
+
+//services
+import { ApiHostService } from "./services/api-host.service";
+import { SystemUtils } from "./services/system.utils";
+
+//components
+import { LandingPageComponent } from "./components/landing-page/landing-page.component";
+import { LoginComponent } from "./components/landing-page/login/login.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { TeacherStaffComponent } from "./components/dashboard/content/list-page/teacher-staff/teacher-staff.component";
+import { ListPageComponent } from "./components/dashboard/content/list-page/list-page.component";
+import { StudentsComponent } from "./components/dashboard/content/list-page/students/students.component";
+import { ContentComponent } from "./components/dashboard/content/content.component";
+import { NavbarComponent } from "./components/dashboard/navbar/navbar.component";
+import { SidebarComponent } from "./components/dashboard/sidebar/sidebar.component";
+import { MainPageComponent } from "./components/dashboard/content/main-page/main-page.component";
 import { ClasslistComponent } from './components/dashboard/content/list-page/classlist/classlist.component';
 
 @NgModule({
@@ -44,9 +52,11 @@ import { ClasslistComponent } from './components/dashboard/content/list-page/cla
     ReactiveFormsModule,
     RouterModule,
     Ng2SearchPipeModule,
-    NgxPaginationModule
+    NgxPaginationModule,
+    // HttpModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ ApiHostService, SystemUtils],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
