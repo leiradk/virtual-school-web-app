a8cb1ad4ce080cb5934c89accaf1fe6f7ecca9b5
capsythe <capslock22.93@gmail.com>
2020-04-13 23:54:50 +0700

    login component fixed

--- src/app/components/landing-page/login/login.component.ts
+++ src/app/components/landing-page/login/login.component.ts
@@ -1,29 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-import { ApiHostService } from '../../../services/api-host.service';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from "@angular/forms";
+import { ApiHostService } from "../../../services/api-host.service";
+import { Router } from "@angular/router";
+
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
-
   public signInForm: FormGroup;
   constructor(
     private apiHost: ApiHostService,
     private fb: FormBuilder,
-
+    private router: Router
   ) {
     this.signInModel();
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   signInModel() {
     this.signInForm = this.fb.group({
       username: [null, Validators.required],
-      password: [null, Validators.required]
-    })
+      password: [null, Validators.required],
+    });
   }
 
   onSubmit() {
@@ -32,10 +37,11 @@ export class LoginComponent implements OnInit {
 
     this.apiHost.signin(value).subscribe((response: any) => {
       if (response) {
-        console.log(response);
+        const { status } = response;
+        if (status === 200) {
+          this.router.navigate(["/dashboard"]);
+        }
       }
-    })
-
+    });
   }
-
 }
