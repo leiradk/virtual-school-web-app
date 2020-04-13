0cd01925df3d40f8c5ce5b256406be00a8f933ce
Melvin Elayron Jr <tyrone.brodoxz@gmail.com>
2020-04-13 18:53:36 +0800

    fixed UI for datatable

--- src/app/components/dashboard/content/list-page/classlist/classlist.component.ts
+++ src/app/components/dashboard/content/list-page/classlist/classlist.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import $ from "jQuery";
 @Component({
-  selector: 'app-classlist',
-  templateUrl: './classlist.component.html',
-  styleUrls: ['./classlist.component.scss']
+  selector: "app-classlist",
+  templateUrl: "./classlist.component.html",
+  styleUrls: ["./classlist.component.scss"],
 })
 export class ClasslistComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+    searchText;
+    public people: any;
+    p: number = 1;
+    viewList: number = 5;
+  
+    constructor() {}
+  
+    ngOnInit(): void {
+      this.people = [
+        {
+          Name: "Tiger",
+          Position: "System Architect",
+          Office: "Edinburgh",
+          Age: "61",
+          StartDate: "2011/04/25",
+          Salary: "$320,800",
+        },
+        {
+          Name: "Lion",
+          Position: "System Architect",
+          Office: "Edinburgh",
+          Age: "61",
+          StartDate: "2011/04/25",
+          Salary: "$320,800",
+        },
+        {
+          Name: "Tiger",
+          Position: "System Architect",
+          Office: "Edinburgh",
+          Age: "61",
+          StartDate: "2011/04/25",
+          Salary: "$320,800",
+        },
+        {
+          Name: "Lion",
+          Position: "System Architect",
+          Office: "Edinburgh",
+          Age: "61",
+          StartDate: "2011/04/25",
+          Salary: "$320,800",
+        },
+        {
+          Name: "Tiger",
+          Position: "System Architect",
+          Office: "Edinburgh",
+          Age: "61",
+          StartDate: "2011/04/25",
+          Salary: "$320,800",
+        },
+        {
+          Name: "Lion",
+          Position: "System Architect",
+          Office: "Edinburgh",
+          Age: "61",
+          StartDate: "2011/04/25",
+          Salary: "$320,800",
+        },
+      ];
+    }
   }
-
-}
+  
--- src/app/components/dashboard/content/list-page/students/students.component.ts
+++ src/app/components/dashboard/content/list-page/students/students.component.ts
@@ -1,20 +1,68 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import $ from "jQuery";
 @Component({
-  selector: 'app-students',
-  templateUrl: './students.component.html',
-  styleUrls: ['./students.component.scss']
+  selector: "app-students",
+  templateUrl: "./students.component.html",
+  styleUrls: ["./students.component.scss"],
 })
 export class StudentsComponent implements OnInit {
+  searchText;
+  public people: any;
+  p: number = 1;
+  viewList: number = 5;
 
-  load = false;
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    // setTimeout(() => {
-    // this.load = true;
-    // }, 1000)
-
+    this.people = [
+      {
+        Name: "Tiger",
+        Position: "System Architect",
+        Office: "Edinburgh",
+        Age: "61",
+        StartDate: "2011/04/25",
+        Salary: "$320,800",
+      },
+      {
+        Name: "Lion",
+        Position: "System Architect",
+        Office: "Edinburgh",
+        Age: "61",
+        StartDate: "2011/04/25",
+        Salary: "$320,800",
+      },
+      {
+        Name: "Tiger",
+        Position: "System Architect",
+        Office: "Edinburgh",
+        Age: "61",
+        StartDate: "2011/04/25",
+        Salary: "$320,800",
+      },
+      {
+        Name: "Lion",
+        Position: "System Architect",
+        Office: "Edinburgh",
+        Age: "61",
+        StartDate: "2011/04/25",
+        Salary: "$320,800",
+      },
+      {
+        Name: "Tiger",
+        Position: "System Architect",
+        Office: "Edinburgh",
+        Age: "61",
+        StartDate: "2011/04/25",
+        Salary: "$320,800",
+      },
+      {
+        Name: "Lion",
+        Position: "System Architect",
+        Office: "Edinburgh",
+        Age: "61",
+        StartDate: "2011/04/25",
+        Salary: "$320,800",
+      },
+    ];
   }
-
 }
