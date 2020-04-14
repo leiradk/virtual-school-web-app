051abe33c2e326c08c2b4ee4cf6214cd6d3888e4
Wilver Deypalubos <Mephistophelles1994@gmail.com>
2020-04-14 19:04:48 +0800

    form builders and Adding Mock Data in students

--- src/app/components/dashboard/content/list-page/students/students.component.ts
+++ src/app/components/dashboard/content/list-page/students/students.component.ts
@@ -1,69 +1,63 @@
 import { Component, OnInit } from "@angular/core";
-import $ from "jQuery";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from "@angular/forms";
 @Component({
   selector: "app-students",
   templateUrl: "./students.component.html",
   styleUrls: ["./students.component.scss"],
 })
 export class StudentsComponent implements OnInit {
+  public addStudentForm: FormGroup;
   searchText;
-  public people: any;
+  public people: any = [];
   p: number = 1;
   viewList: number = 5;
 
-  constructor() {}
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.studentFormModel();
+  }
 
   ngOnInit(): void {
+
+    this.mockData();
+  }
+  studentFormModel() {
+    this.addStudentForm = this.fb.group({
+      email: [null, [Validators.required, Validators.email]],
+      firstname: [null, Validators.required],
+      middlename: [null, Validators.required],
+      lastname: [null, Validators.required],
+      password: [null, [Validators.required, Validators.minLength(6)]],
+      repassword: [null, [Validators.required, Validators.minLength(6)]],
+    });
+  }
+  onSubmit(){
+    console.log(this.addStudentForm);
+    const { value } = this.addStudentForm;
+
+    const data = {
+      email: value.email,
+      name: `${value.firstname} ${value.middlename} ${value.lastname}`,
+      status: 'student'
+    }
+    this.people.push(data);
+  }
+  mockData(){
     this.people = [
       {
-        Name: "Tiger",
-        Position: "System Architect",
-        Office: "Edinburgh",
-        Age: "61",
-        StartDate: "2011/04/25",
-        Salary: "$320,800",
+        email: "sample@gmail.com",
+        name: "Tiger",
+        status: "Student",
+   
       },
-      {
-        Name: "Lion",
-        Position: "System Architect",
-        Office: "Edinburgh",
-        Age: "61",
-        StartDate: "2011/04/25",
-        Salary: "$320,800",
-      },
-      {
-        Name: "Tiger",
-        Position: "System Architect",
-        Office: "Edinburgh",
-        Age: "61",
-        StartDate: "2011/04/25",
-        Salary: "$320,800",
-      },
-      {
-        Name: "Lion",
-        Position: "System Architect",
-        Office: "Edinburgh",
-        Age: "61",
-        StartDate: "2011/04/25",
-        Salary: "$320,800",
-      },
-      {
-        Name: "Tiger",
-        Position: "System Architect",
-        Office: "Edinburgh",
-        Age: "61",
-        StartDate: "2011/04/25",
-        Salary: "$320,800",
-      },
-      {
-        Name: "Lion",
-        Position: "System Architect",
-        Office: "Edinburgh",
-        Age: "61",
-        StartDate: "2011/04/25",
-        Salary: "$320,800",
-      },
+  
     ];
-
   }
+  
 }
