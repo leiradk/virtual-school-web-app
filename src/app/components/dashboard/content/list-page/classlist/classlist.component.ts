8eb7c36d0281120521e91ba0ae3d38b3359d8111
Wilver Deypalubos <Mephistophelles1994@gmail.com>
2020-04-15 21:24:05 +0800

    Cllass List Forms setup

--- src/app/components/dashboard/content/list-page/classlist/classlist.component.ts
+++ src/app/components/dashboard/content/list-page/classlist/classlist.component.ts
@@ -1,70 +1,65 @@
 import { Component, OnInit } from "@angular/core";
-import $ from "jQuery";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from "@angular/forms";
 @Component({
   selector: "app-classlist",
   templateUrl: "./classlist.component.html",
   styleUrls: ["./classlist.component.scss"],
 })
 export class ClasslistComponent implements OnInit {
+  public addClassForm: FormGroup;
 
-    searchText;
-    public people: any;
-    p: number = 1;
-    viewList: number = 5;
-  
-    constructor() {}
-  
-    ngOnInit(): void {
-      this.people = [
-        {
-          Name: "Tiger",
-          Position: "System Architect",
-          Office: "Edinburgh",
-          Age: "61",
-          StartDate: "2011/04/25",
-          Salary: "$320,800",
-        },
-        {
-          Name: "Lion",
-          Position: "System Architect",
-          Office: "Edinburgh",
-          Age: "61",
-          StartDate: "2011/04/25",
-          Salary: "$320,800",
-        },
-        {
-          Name: "Tiger",
-          Position: "System Architect",
-          Office: "Edinburgh",
-          Age: "61",
-          StartDate: "2011/04/25",
-          Salary: "$320,800",
-        },
-        {
-          Name: "Lion",
-          Position: "System Architect",
-          Office: "Edinburgh",
-          Age: "61",
-          StartDate: "2011/04/25",
-          Salary: "$320,800",
-        },
-        {
-          Name: "Tiger",
-          Position: "System Architect",
-          Office: "Edinburgh",
-          Age: "61",
-          StartDate: "2011/04/25",
-          Salary: "$320,800",
-        },
-        {
-          Name: "Lion",
-          Position: "System Architect",
-          Office: "Edinburgh",
-          Age: "61",
-          StartDate: "2011/04/25",
-          Salary: "$320,800",
-        },
-      ];
-    }
+  searchText;
+  public people: any;
+  p: number = 1;
+  viewList: number = 5;
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.studentFormModel();
+   }
+
+  ngOnInit(): void {
+    this.mockData();
   }
-  
+  get name() {
+    return this.addClassForm.get('name') as FormControl;
+  } 
+  get level() {
+    return this.addClassForm.get('level') as FormControl;
+  }
+  get section() {
+    return this.addClassForm.get('section') as FormControl;
+  }
+  get subject() {
+    return this.addClassForm.get('subject') as FormControl;
+  }
+  studentFormModel() {
+    this.addClassForm = this.fb.group({
+      name: [null, Validators.required],
+      level: [null, Validators.required],
+      section: [null, Validators.required],
+      subject: [null, Validators.required],
+   
+    });
+  }
+onSubmit() {
+
+}
+  mockData() {
+    this.people = [
+      {
+        Name: "Tiger",
+        level: "Grade 11",
+        section: "Section A",
+        subject: "Math",
+      },
+
+    ];
+  }
+}
--- src/app/components/dashboard/content/list-page/subject/subject.component.ts
+++ src/app/components/dashboard/content/list-page/subject/subject.component.ts
@@ -1,70 +1,87 @@
 import { Component, OnInit } from "@angular/core";
-import $ from "jQuery";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from "@angular/forms";
+import { ToastrService } from "ngx-toastr";
+
 @Component({
   selector: "app-subject",
   templateUrl: "./subject.component.html",
   styleUrls: ["./subject.component.scss"],
 })
 export class SubjectComponent implements OnInit {
-
+  public addStudentForm: FormGroup;
     searchText;
     public people: any;
     p: number = 1;
     viewList: number = 5;
   
-    constructor() {}
+    constructor(
+      private fb: FormBuilder,
+      private toastr: ToastrService
+    ) {
+      this.studentFormModel();
+    }
   
     ngOnInit(): void {
+      this.mockData();
+    }
+
+    studentFormModel() {
+      this.addStudentForm = this.fb.group({
+        email: [null, [Validators.required, Validators.email]],
+        firstname: [null, Validators.required],
+        middlename: [null, Validators.required],
+        lastname: [null, Validators.required],
+        password: [null, [Validators.required, Validators.minLength(6)]],
+        repassword: [null, [Validators.required, Validators.minLength(6)]],
+      });
+    }
+    onSubmit(){
+      console.log(this.addStudentForm);
+      const { value } = this.addStudentForm;
+  
+      const data = {
+        email: value.email,
+        name: `${value.firstname} ${value.middlename} ${value.lastname}`,
+        status: 'student'
+      }
+      this.people.push(data);
+      // setTimeout(() => {
+      //   this.showSuccess();
+      // }, 1000);
+    }
+    mockData(){
       this.people = [
         {
-          Name: "Tiger",
-          Position: "System Architect",
-          Office: "Edinburgh",
-          Age: "61",
-          StartDate: "2011/04/25",
-          Salary: "$320,800",
+          email: "sample@gmail.com",
+          name: "Tiger",
+          status: "Student",
+     
         },
-        {
-          Name: "Lion",
-          Position: "System Architect",
-          Office: "Edinburgh",
-          Age: "61",
-          StartDate: "2011/04/25",
-          Salary: "$320,800",
-        },
-        {
-          Name: "Tiger",
-          Position: "System Architect",
-          Office: "Edinburgh",
-          Age: "61",
-          StartDate: "2011/04/25",
-          Salary: "$320,800",
-        },
-        {
-          Name: "Lion",
-          Position: "System Architect",
-          Office: "Edinburgh",
-          Age: "61",
-          StartDate: "2011/04/25",
-          Salary: "$320,800",
-        },
-        {
-          Name: "Tiger",
-          Position: "System Architect",
-          Office: "Edinburgh",
-          Age: "61",
-          StartDate: "2011/04/25",
-          Salary: "$320,800",
-        },
-        {
-          Name: "Lion",
-          Position: "System Architect",
-          Office: "Edinburgh",
-          Age: "61",
-          StartDate: "2011/04/25",
-          Salary: "$320,800",
-        },
+    
       ];
+    }
+    get email() {
+      return this.addStudentForm.get('email') as FormControl;
+    } 
+    get firstname() {
+      return this.addStudentForm.get('firstname') as FormControl;
+    }
+    get middlename() {
+      return this.addStudentForm.get('middlename') as FormControl;
+    }
+    get lastname() {
+      return this.addStudentForm.get('lastname') as FormControl;
+    }
+    get password() {
+      return this.addStudentForm.get('password') as FormControl;
+    }
+    get repassword() {
+      return this.addStudentForm.get('repassword') as FormControl;
     }
   }
   
