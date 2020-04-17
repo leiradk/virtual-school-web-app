f2710f48df2605f247ee7872007ee7b4bae2d270
Wilver Deypalubos <mephistophelles1994@gmail.com>
2020-04-17 14:44:20 +0800

    Routing Changed and Teacher Page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,18 +22,21 @@ import { SystemUtils } from "./services/system.utils";
 import { LandingPageComponent } from "./components/landing-page/landing-page.component";
 import { LoginComponent } from "./components/landing-page/login/login.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
-import { TeacherStaffComponent } from "./components/dashboard/content/list-page/teacher-staff/teacher-staff.component";
-import { ListPageComponent } from "./components/dashboard/content/list-page/list-page.component";
-import { StudentsComponent } from "./components/dashboard/content/list-page/students/students.component";
-import { ContentComponent } from "./components/dashboard/content/content.component";
-import { NavbarComponent } from "./components/dashboard/navbar/navbar.component";
-import { SidebarComponent } from "./components/dashboard/sidebar/sidebar.component";
-import { MainPageComponent } from "./components/dashboard/content/main-page/main-page.component";
-import { ClasslistComponent } from './components/dashboard/content/list-page/classlist/classlist.component';
-import { ParentListComponent } from './components/dashboard/content/list-page/parent-list/parent-list.component';
-import { SubjectComponent } from './components/dashboard/content/list-page/subject/subject.component';
+import { TeacherStaffComponent } from "./components/dashboard/admin/content/list-page/teacher-staff/teacher-staff.component";
+import { ListPageComponent } from "./components/dashboard/admin/content/list-page/list-page.component";
+import { StudentsComponent } from "./components/dashboard/admin/content/list-page/students/students.component";
+import { ContentComponent } from "./components/dashboard/admin/content/content.component";
+import { NavbarComponent } from "./components/dashboard/admin/navbar/navbar.component";
+import { SidebarComponent } from "./components/dashboard/admin/sidebar/sidebar.component";
+import { MainPageComponent } from "./components/dashboard/admin/content/main-page/main-page.component";
+import { ClasslistComponent } from './components/dashboard/admin/content/list-page/classlist/classlist.component';
+import { ParentListComponent } from './components/dashboard/admin/content/list-page/parent-list/parent-list.component';
+import { SubjectComponent } from './components/dashboard/admin/content/list-page/subject/subject.component';
 import { LoadingSpinnerComponent } from './components/ui/loading-spinner/loading-spinner.component';
-
+import { TeacherComponent } from './components/dashboard/teacher/teacher.component';
+import { TeacherContentComponent } from './components/dashboard/teacher/teacher-content/teacher-content.component';
+import { TeacherSidebarComponent } from './components/dashboard/teacher/teacher-sidebar/teacher-sidebar.component';
+import { TeacherNavbarComponent } from './components/dashboard/teacher/teacher-navbar/teacher-navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -50,7 +53,11 @@ import { LoadingSpinnerComponent } from './components/ui/loading-spinner/loading
     ClasslistComponent,
     ParentListComponent,
     SubjectComponent,
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    TeacherComponent,
+    TeacherContentComponent,
+    TeacherSidebarComponent,
+    TeacherNavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/dashboard/teacher/teacher-navbar/teacher-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-navbar',
+  templateUrl: './teacher-navbar.component.html',
+  styleUrls: ['./teacher-navbar.component.scss']
+})
+export class TeacherNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
