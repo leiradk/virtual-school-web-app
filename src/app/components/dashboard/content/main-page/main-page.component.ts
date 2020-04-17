6143ece6defcd94c243e767647eae6a92c1a0d90
Wilver Deypalubos <mephistophelles1994@gmail.com>
2020-04-17 13:53:09 +0800

    Admin access restriction without logging in

--- src/app/components/dashboard/content/main-page/main-page.component.ts
+++ src/app/components/dashboard/content/main-page/main-page.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { SystemUtils } from '../../../../services/system.utils';
+import { Component, OnInit } from "@angular/core";
+import { SystemUtils } from "../../../../services/system.utils";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-main-page',
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  selector: "app-main-page",
+  templateUrl: "./main-page.component.html",
+  styleUrls: ["./main-page.component.scss"],
 })
 export class MainPageComponent implements OnInit {
-
-  constructor(
-    private system: SystemUtils
-  ) { }
+  data: any = [];
+  constructor(private system: SystemUtils, private router: Router) {}
 
   ngOnInit(): void {
-    const data = this.system.retrieveItem("userData");
-    console.log(data);
+    this.data = this.system.retrieveItem("userData");
+    console.log(this.data);
+    if (this.data === undefined || this.data === null) {
+      this.router.navigate(["/Landing-Page"]);
+    } else {
+      const { token } = this.data;
+      if (token === undefined) {
+        this.router.navigate(["/Landing-Page"]);
+      }
+    }
   }
-
 }
