5e3c1ba49d45a131881064f5d61f76f59833b0eb
Wilver Deypalubos <Mephistophelles1994@gmail.com>
2020-04-22 01:21:41 +0800

    added component and mock ui for student list

---
+++ src/app/components/dashboard/teacher/teacher-content/class-details/check-students/check-students.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-check-students',
+  templateUrl: './check-students.component.html',
+  styleUrls: ['./check-students.component.scss']
+})
+export class CheckStudentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
