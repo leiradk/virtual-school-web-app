7661190c79c1cbdb914a885499991ae41e686916
Wilver Deypalubos <Mephistophelles1994@gmail.com>
2020-04-29 19:24:26 +0800

    setup mock data on post side

--- src/app/components/dashboard/teacher/teacher-content/class-details/class-home/class-post/class-post.component.ts
+++ src/app/components/dashboard/teacher/teacher-content/class-details/class-home/class-post/class-post.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, HostListener } from '@angular/core';
-
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from "@angular/forms";
 @Component({
   selector: 'app-class-post',
   templateUrl: './class-post.component.html',
@@ -9,7 +14,13 @@ import { Component, OnInit, HostListener } from '@angular/core';
 export class ClassPostComponent implements OnInit {
   myFocusVar: any = false;
   comments: any = [];
-  constructor() { }
+  public postForm: FormGroup;
+  loading = false;
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.postModel();
+  }
 
   isSticky: boolean = false;
 
@@ -25,8 +36,29 @@ export class ClassPostComponent implements OnInit {
     ]
   }
 
+  postModel() {
+    this.postForm = this.fb.group({
+      postContent: [null, Validators.required],
+    });
+  }
+  get postContent() {
+    return this.postForm.get('postContent') as FormControl;
+  }
   @HostListener('window:scroll', ['$event'])
   checkScroll() {
     this.isSticky = window.pageYOffset >= 250;
+  }
+
+  onSubmit() {
+    const { value } = this.postForm;
+    console.log(value);
+    const payload = [{
+      user: "Melvin Elayron",
+      comment: value.postContent
+    }];
+    for (let i = 0; i <= this.comments.length; i++){
+      payload.push(this.comments[i]);
+    }
+    this.comments = payload;
   }
 }
