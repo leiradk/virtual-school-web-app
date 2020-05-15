a8d5683fcbca422e765e9d1e4f8016f22645f334
Wilver Deypalubos <Mephistophelles1994@gmail.com>
2020-05-15 16:58:28 +0800

    logout problem fixed

--- src/app/components/dashboard/user/user-navbar/user-navbar.component.ts
+++ src/app/components/dashboard/user/user-navbar/user-navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from "@angular/router";
+import { SystemUtils } from '../../../../services/system.utils';
 @Component({
   selector: 'app-user-navbar',
   templateUrl: './user-navbar.component.html',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserNavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private system: SystemUtils
+  ) { }
 
   ngOnInit(): void {
   }
+  logout() {
+    this.router.navigate(["/landing-Page/login"]);
+    this.system.deleteKey('userData');
+    this.system.deleteKey('classDetails');
+    this.system.deleteKey('workDetails');
 
+  }
 }
