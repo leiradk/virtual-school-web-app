f57fbad7c955eaf97ecb1dd26eb25d4d393258ff
Wilver Deypalubos <Mephistophelles1994@gmail.com>
2020-05-27 19:36:54 +0800

    observable added on the services of the archive

--- src/app/services/admin-list-data.service.ts
+++ src/app/services/admin-list-data.service.ts
@@ -8,14 +8,23 @@ export class AdminListDataService {
 
   private studentParams = new BehaviorSubject<string>(null);
   private teacherParams = new BehaviorSubject<string>(null);
-  private parentParams = new BehaviorSubject<string>(null);
+  private parentParams = new BehaviorSubject<string>(null); 
+  private inactiveStudentParams = new BehaviorSubject<string>(null);
+  private inactiveTeacherParams = new BehaviorSubject<string>(null);
+  private inactiveParentParams = new BehaviorSubject<string>(null);
   student: Observable<string>;
   teacher: Observable<string>;
   parent: Observable<string>;
+  inactiveStudent: Observable<string>;
+  inactiveTeacher: Observable<string>;
+  inactiveParent: Observable<string>;
   constructor() {
     this.student = this.studentParams.asObservable();
     this.teacher = this.teacherParams.asObservable();
     this.parent = this.teacherParams.asObservable();
+    this.inactiveStudent = this.inactiveStudentParams.asObservable();
+    this.inactiveTeacher = this.inactiveTeacherParams.asObservable();
+    this.inactiveParent = this.inactiveParentParams.asObservable();
   }
 
   setStudent(token) {
@@ -28,5 +37,16 @@ export class AdminListDataService {
 
   setParents(comments) {
     this.parentParams.next(comments);
+  }
+  setInactiveStudent(token) {
+    this.inactiveStudentParams.next(token);
+  }
+
+  setInactiveTeacher(comments) {
+    this.inactiveTeacherParams.next(comments);
+  }
+
+  setInactiveParents(comments) {
+    this.inactiveParentParams.next(comments);
   }
 }
