1d8ce37709de120c811c906c384cecb386b5312d
Wilver Deypalubos <Mephistophelles1994@gmail.com>
2020-06-12 20:24:11 +0800

    Update user on header change status and name depending on the user

--- src/app/components/dashboard/user/user-navbar/user-navbar.component.ts
+++ src/app/components/dashboard/user/user-navbar/user-navbar.component.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { SystemUtils } from '../../../../services/system.utils';
+
 @Component({
   selector: 'app-user-navbar',
   templateUrl: './user-navbar.component.html',
   styleUrls: ['./user-navbar.component.scss', '../../../../../assets/admin/css/styles.min.css']
 })
 export class UserNavbarComponent implements OnInit {
-
+  userData: any;
+  username: any;
+  usertype: any;
   constructor(
     private router: Router,
     private system: SystemUtils
   ) { }
 
   ngOnInit(): void {
+    this.userData = this.system.retrieveItem('userData');
+    const { username, usertype } = this.userData.data;
+    this.username = username;
+    console.log(usertype)
+    
+    if (parseInt(usertype) === 10002) {
+      this.usertype = 'Teacher';
+    } else if(parseInt(usertype) === 10003) {
+      this.usertype = 'Student';
+    } else {
+      console.log('not accepted')
+    }
+
   }
   logout() {
     console.log('logout')
