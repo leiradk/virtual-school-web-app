e29344e917668931b8e17c6fd69203fa9033f1e8
Melvin Elayron <tyrone.brodoxz@gmail.com>
2020-08-13 20:30:10 +0800

    classwork ui fix on parent-side

--- src/app/components/dashboard/user/room-details/parent-side/check-student-classwork/check-student-classwork.component.ts
+++ src/app/components/dashboard/user/room-details/parent-side/check-student-classwork/check-student-classwork.component.ts
@@ -29,6 +29,49 @@ export class CheckStudentClassworkComponent implements OnInit {
   }
 
 
+  classWorkStyle(value: any) {
+    if (value % 2 == 0) {
+      return 'reminder-butt2'
+    } else {
+      return 'reminder-butt1'
+    }
+  }
+
+  getDate(submitted) {
+    const dateTime = submitted;
+    const dateTimeSplit = dateTime.split(' ');
+    const date = dateTimeSplit[0].split('-');
+    const month = this.getMonth(parseInt(date[1]));
+    return month + ' ' + date[2] + ', ' + date[0];
+  }
+  getMonth(month) {
+    if (month === 1) {
+      return 'January';
+    } else if (month === 2) {
+      return 'February'
+    } else if (month === 3) {
+      return 'March'
+    } else if (month === 4) {
+      return 'April'
+    } else if (month === 5) {
+      return 'May'
+    } else if (month === 6) {
+      return 'June'
+    } else if (month === 7) {
+      return 'July'
+    } else if (month === 8) {
+      return 'August'
+    } else if (month === 9) {
+      return 'September'
+    } else if (month === 10) {
+      return 'October'
+    } else if (month === 11) {
+      return 'November'
+    } else if (month === 12) {
+      return 'December'
+    }
+  }
+
   getClasswork() {
     this.apiHost.getStudentClasswork(this.userdata.token, this.studentdata.userID, this.roomdata.classID)
       .subscribe((response: any) => {
