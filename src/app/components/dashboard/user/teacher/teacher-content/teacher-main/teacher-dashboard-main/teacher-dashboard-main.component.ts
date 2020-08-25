2cf3b45ee45c946184adaba1d0b6e5b91747d055
Wilver Deypalubos <Mephistophelles1994@gmail.com>
2020-08-25 14:21:05 +0800

    setup count function on dashbord

--- src/app/components/dashboard/user/teacher/teacher-content/teacher-main/teacher-dashboard-main/teacher-dashboard-main.component.ts
+++ src/app/components/dashboard/user/teacher/teacher-content/teacher-main/teacher-dashboard-main/teacher-dashboard-main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiHostService } from '../../../../../../../services/api-host.service';
+import { SystemUtils } from '../../../../../../../services/system.utils';
 @Component({
   selector: 'app-teacher-dashboard-main',
   templateUrl: './teacher-dashboard-main.component.html',
@@ -7,9 +8,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeacherDashboardMainComponent implements OnInit {
 
-  constructor() { }
+  totalStudentCount: any = 0;
+  totalClassesCount: any = 0;
+  totalParentCount: any = 0;
+  constructor(
+    private apiHost: ApiHostService,
+    private system: SystemUtils,
+  ) { }
 
   ngOnInit(): void {
+    const { token } = this.system.retrieveItem('userData');
+    this.getTotalStudents(token);
+    this.getTotalClasses(token);
+    this.getTotalParents(token);
   }
 
+  getTotalStudents(token) {
+    this.apiHost.teacherGetTotalStudents(token)
+      .subscribe((response: any) => {
+        console.log('totalStudents:', response)
+        const { students } = response.body[0]; 
+        console.log(students)
+        this.totalStudentCount = students
+      }, (error: any) => {
+        console.log('totalStudentsError:', error)
+        if(error.status === 404) {
+          if(error.statusText === "Not Found") {
+            this.totalStudentCount = 0;
+          }
+        }
+      })
+  }
+  getTotalClasses(token) {
+    this.apiHost.teacherGetTotalClasses(token)
+    .subscribe((response: any) => {
+      console.log('totalClasses:', response)
+      const { classes } = response.body[0]; 
+      console.log(classes)
+      this.totalClassesCount = classes
+    }, (error: any) => {
+      console.log('totalStudentsError:', error)
+      if(error.status === 404) {
+        if(error.statusText === "Not Found") {
+          this.totalClassesCount = 0;
+        }
+      }
+    })
+  }
+  getTotalParents(token) {
+    this.apiHost.teacherGetTotalParents(token)
+    .subscribe((response: any) => {
+      console.log('totalParents:', response)
+      const { students } = response.body[0]; 
+      console.log(students)
+      this.totalParentCount = students
+    }, (error: any) => {
+      console.log('totalStudentsError:', error)
+      if(error.status === 404) {
+        if(error.statusText === "Not Found") {
+          this.totalParentCount = 0;
+        }
+      }
+    })
+  }
 }
